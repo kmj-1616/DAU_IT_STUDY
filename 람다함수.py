e4f7c5df90efd7b00b535ef63d596ee0aa76c686
권미정 <84905321+kmj-1616@users.noreply.github.com>
2022-04-19 18:28:34 +0900

    Update and rename prg01.py to 람다함수.py

--- 람다함수.py
+++ 람다함수.py
@@ -5,8 +5,5 @@ def avg(data):
 d=[20,15,18,20]
 print(avg(d)) #결과: 18.25
 
-s1=tuple(map(str,d))
-print(s1)
-
 avg1=lambda data:sum(data)/len(data) #람다함수로 바꾸기: 함수를 한 줄로
 print(avg1(d)) #결과: 18.25 로 같음
