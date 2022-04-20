11b25e56903e1ce432e53c21ba6d372da28e6087
권미정 <84905321+kmj-1616@users.noreply.github.com>
2022-04-20 18:07:22 +0900

    Update lcm.py

--- lcm.py
+++ lcm.py
@@ -1,7 +1,7 @@
 from math import gcd #표준라이브러리 math에서 gcd 호출
 
 def lcm(a,b):
-    return a*b // gcd(a,b) #a*b/a와 b의 최대공약수가 a,의 최소공배수
+    return a*b // gcd(a,b) #a*b/a와 b의 최대공약수가 a,b의 최소공배수
 
 print(lcm(10,12))
 #결과 60
