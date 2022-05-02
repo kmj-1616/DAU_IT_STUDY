8f60d957e7c659d46d68c11b825cabb11a7df967
권미정 <84905321+kmj-1616@users.noreply.github.com>
2022-05-02 23:23:02 +0900

    Update numpy_sum.py

--- numpy_sum.py
+++ numpy_sum.py
@@ -1,6 +1,10 @@
 import numpy as np
 data=np.array([[3,5,10],[8,10,12],[7,10,23]])
 print(data.sum()) #전체의 합
+#결과: 88
 print(data.sum(axis=0)) #행의 합
+#결과: [18 25 45]
 print(data.sum(axis=1)) #열의 합
+#결과: [18 30 40]
 print(data.std(axis=0)) #행의 표준편차
+#결과: [2.1602469  2.3570226  5.71547607]
