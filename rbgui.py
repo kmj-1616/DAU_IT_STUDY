72058a51cf1e59030533f792b7d179f8e548ef97
권미정 <84905321+kmj-1616@users.noreply.github.com>
2022-06-04 23:40:16 +0900

    Add files via upload

---
+++ rbgui.py
@@ -0,0 +1,38 @@
+from tkinter import *
+from tkinter import messagebox
+def fun1() :
+    ans=messagebox.askyesno("확인","프로그램을 종료하시겠습니까?")
+    if ans :
+        w.destroy() #창을 닫고,메모리에서 지워줌
+    else :
+        w.title("취소하였습니다")
+def funCheck() :
+    if chk.get()==1:
+        lbl.configure(text="재미있어요")
+    else:
+        lbl.configure(text="어려워요")
+def funRadio() :
+    if var.get()==1 :
+        lbl.configure(text="파이썬을 선택하셨습니다")
+    elif var.get()==2 :
+        lbl.configure(text="웹프로그래밍을 선택하셨습니다")
+    else:
+        lbl.configure(text="데이터베이스를 선택하셨습니다")
+w=Tk()
+w.title("20 권미정 (이벤트)")
+w.geometry("400x250")
+chk=IntVar()
+cb=Checkbutton(w,text="파이썬은 재미있다",variable=chk,command=funCheck)
+var=IntVar()
+rb1=Radiobutton(w,text="프로그래밍 언어",variable=var,value=1,command=funRadio)
+rb2=Radiobutton(w,text="웹프로그래밍",variable=var,value=2,command=funRadio)
+rb3=Radiobutton(w,text="데이터베이스",variable=var,value=3,command=funRadio)
+lbl=Label(w,text="",fg='red')
+btn=Button(w,text="확인",command=fun1)
+cb.pack()
+rb1.pack()
+rb2.pack()
+rb3.pack()
+btn.pack()
+lbl.pack(side=BOTTOM) #라벨이 아래에 배치
+w.mainloop()
